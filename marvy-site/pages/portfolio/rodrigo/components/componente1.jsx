db03045bf110cdb7b0424ce08a6f603b5f951863
Victor Mexicano <vmmexicano@gmail.com>
2021-12-02 15:03:34 -0600

    Initialize project

---
+++ marvy-site/pages/portfolio/rodrigo/components/componente1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Componente1 = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse ipsam ea, debitis rem enim aperiam quisquam minima ratione assumenda amet veniam sunt harum fuga, animi corporis recusandae tempora doloremque reprehenderit.
+    </div>
+  )
+}
+
+export default Componente1
---
+++ marvy-site/pages/portfolio/rodrigo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Componente1 from './components/componente1'
+
+const index = () => {
+  return (
+    <div>
+      Aqui va tu portafolio rodrigo
+      <Componente1 />
+    </div>
+  )
+}
+
+export default index
